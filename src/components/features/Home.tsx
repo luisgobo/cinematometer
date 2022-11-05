35ee13dd93277a4df1b2a1984869ceaa9e6ab303
luisgobo <lagbtech@hotmail.com>
2022-11-04 20:19:51 -0600

    Updated movies info

--- src/components/features/Home.tsx
+++ src/components/features/Home.tsx
@@ -3,63 +3,54 @@ import { useFierbase } from "../../context/use-firebase";
 import { useMovies } from "../../context/use-movies";
 import ProtectedPage from "../layouts/ProtectedPage";
 import { InfoCarousel } from "../organisms/Carousel/InfoCarousel";
-//import {isMobile} from 'react-device-detect';
-import UAParser from "ua-parser-js";
 import { Movie } from "../../models/movie";
-
-declare module 'http' {
-    interface IncomingHttpHeaders {
-        "XYZ-Token"?: string
-    }
-}
 
 export const Home = () => {
 
-  const [deviceType, setDeviceType] = React.useState("");
   const { appUser } = useFierbase();
-  const { nowPlayingMovies, 
-          nowPlayingMoviesStatus,
-          popularMoviesStatus,
-          popularMovies
-        } = useMovies();
+  const {
+    nowPlayingMovies,
+    popularMovies,
+    topRatedMovies,
+    upComingMovies,
 
+    nowPlayingMoviesStatus,
+    popularMoviesStatus,
+    topRatedMoviesStatus,
+    upComingMoviesStatus,
 
+  } = useMovies();
 
-  React.useEffect(() => {
-
-    const userAgent = navigator.userAgent;
-    const parser = new UAParser();
-    parser.setUA(userAgent);
-    const result = parser.getResult();
-    const deviceType = (result.device && result.device.type) || "desktop";    
-    setDeviceType(deviceType);
-
-  }, [])
-
-
-function processMovieList(movies: Movie[] | undefined ){
-  return movies === undefined ? [] : movies;
-}
+  function processMovieList(movies: Movie[] | undefined) {
+    return movies === undefined ? [] : movies;
+  }
 
   return (
     <ProtectedPage>
-      <h1>Home Page</h1>
-      <p>hola {appUser?.name}</p>
+      <h1>Cinematometer</h1>
+      <p>Hi {appUser?.name}!</p>
       {
         <div>
-            <InfoCarousel 
-              title={"Now in theatres"} 
-              movieList={processMovieList(nowPlayingMovies)} 
-              listStatus={nowPlayingMoviesStatus} 
-              deviceType={deviceType} 
-            />            
-          {/* <InfoCarousel 
-            title={"Popular movies"} 
-            movieList={popularMovies} 
-            listStatus={popularMoviesStatus} 
-            deviceType={deviceType}
-          /> */}
-          
+          <InfoCarousel
+            title={"Now in theatres"}
+            movieList={processMovieList(nowPlayingMovies)}
+            listStatus={nowPlayingMoviesStatus}
+          />
+          <InfoCarousel
+            title={"Popular movies"}
+            movieList={processMovieList(popularMovies)}
+            listStatus={popularMoviesStatus}
+          />
+          <InfoCarousel
+            title={"Top Rated movies"}
+            movieList={processMovieList(topRatedMovies)}
+            listStatus={topRatedMoviesStatus}
+          />
+          <InfoCarousel
+            title={"Upcoming movies"}
+            movieList={processMovieList(upComingMovies)}
+            listStatus={upComingMoviesStatus}
+          />
         </div>
       }
     </ProtectedPage>
